cd5a3fb09a703f70fe52c28dfa48cecabf37e9cb
Tahmeedzam <tahmeedzamindar.tz@gmail.com>
2025-06-03 14:06:42 +0530

    Testing commit

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -16,7 +16,7 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Center(
           child: Text(
-            'Algo-Verse',
+            'Algo-Verse for life',
             style: TextStyle(
               fontSize: 18,
               color: Color(0xffE0E0E0),
